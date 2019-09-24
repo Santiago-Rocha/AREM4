6baf4d2815f1ce04945d62a8bfbb4c83243c2cd4
Santiago-Rocha <santiago.rocha@tulpep.com>
2019-09-24 12:09:29 -0500

    mod README

--- src/main/java/edu/escuelaing/arem/project/Threads/ThreadPool.java
+++ src/main/java/edu/escuelaing/arem/project/Threads/ThreadPool.java
@@ -10,7 +10,9 @@ public class ThreadPool {
     public static ServerSocket serverSocket = AppSocket.StartServerSocket();
     public static ExecutorService exService = Executors.newCachedThreadPool();
         
-
+    /**
+     * Este metodo mantiene un socket escuchando peticiones, al recibir una nueva delega el trabajo al pool de hilos
+     */
     public static void start() {
         while (true) {
             Socket clientSocket = AppSocket.StartClientSocket(serverSocket);
